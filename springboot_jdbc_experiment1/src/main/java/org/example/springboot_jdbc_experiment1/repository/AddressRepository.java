1439617f83549983977e86e4e9ef2238af68f456
Zn952 <14769933+Zn952@user.noreply.gitee.com>
2024-09-25 15:11:43 +0800

    feature/experient1

--- springboot_jdbc_experiment1/src/main/java/org/example/springboot_jdbc_experiment1/repository/AddressRepository.java
+++ springboot_jdbc_experiment1/src/main/java/org/example/springboot_jdbc_experiment1/repository/AddressRepository.java
@@ -1,7 +1,6 @@
 package org.example.springboot_jdbc_experiment1.repository;
 
 import org.example.springboot_jdbc_experiment1.dox.Address;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +11,7 @@ import java.util.List;
 public interface AddressRepository extends CrudRepository<Address,String> {
     @Query(
             """
-             select * from address a 
+             select * from address a
          where a.user_id=:userId"""
     )
     List<Address> findByUserId(String userId);
